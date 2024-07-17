222865f105e96cdd42bf467d0a7a5a128b35aef8
Jesse Soo <jesse.soo.my@gmail.com>
2024-07-17 15:27:28 +0800

    Update Figma URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const GITHUB_REPO_URL = "https://github.com/govtechmy/myds";
 export const FIGMA_URL =
-  "https://www.figma.com/design/42LLT8iBDAxNuiMmu4vxxa/Portal-MYDS";
+  "https://www.figma.com/design/svmWSPZarzWrJ116CQ8zpV/MYDS-(Beta)?node-id=7-20696&t=A1mfwrtKyez4lJjE-1";
 export const FEEDBACK_EMAIL = "design@tech.gov.my";
