92c0aef9237ebd9de4cc457e7184a33cf8751f18
Jesse Soo <jesse.soo.my@gmail.com>
2024-08-05 17:29:21 +0800

    Clean up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,21 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const toRM = (value: number | null): string => {
-  if (value === null) return "-";
-  return new Intl.NumberFormat("ms-MY", {
-    style: "currency",
-    currency: "MYR",
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-  }).format(value);
-};
-
-export const toComma = (value: number | null): string => {
-  if (value === null) return "-";
-  return new Intl.NumberFormat("ms-MY", {
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-  }).format(value);
-};
