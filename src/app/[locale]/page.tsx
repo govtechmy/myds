8658b611fe9bf148a32f3781e229c0374bca2017
Jesse Soo <jesse.soo.my@gmail.com>
2024-08-12 14:02:55 +0800

    Use a component to wrap every page components in <article>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,6 +7,7 @@ import { type MetadataProps } from "@/lib/page";
 import type { Metadata } from "next";
 import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
+import React, { ReactNode } from "react";
 
 const TIMELINE_ITEMS = 7;
 
@@ -47,51 +48,52 @@ export default function Home() {
   const t = useTranslations();
 
   return (
-    <main className="flex w-full flex-col divide-y divide-washed-100 lg:py-0">
-      {/* Wrapper container components in <article> to display full-width dividers */}
-      <article>
-        <Hero />
-      </article>
-      <article>
-        <About id="about" className="py-[3rem] md:py-[5.25rem]" />
-      </article>
+    <Main>
+      <Hero />
+      <About id="about" className="py-[3rem] md:py-[5.25rem]" />
       {previewItems.map((item, i) => (
-        <article key={i}>
-          <PreviewContent
-            className="py-[3rem] md:py-[5.25rem]"
-            data={{
-              tag: t(item.tagKey),
-              title: t(item.titleKey),
-              description: t(item.descriptionKey),
-              img: {
-                ...item.img,
-                alt: t(item.img.altKey),
-              },
-            }}
-            reversed={(i + 1) % 2 === 0}
-          />
-        </article>
-      ))}
-      <article>
-        <Timeline
-          id="ourplan"
-          cover={{
-            tag: t("Home.timeline.cover.tag"),
-            title: t("Home.timeline.cover.title"),
-            description: t("Home.timeline.cover.description"),
+        <PreviewContent
+          key={i}
+          className="py-[3rem] md:py-[5.25rem]"
+          data={{
+            tag: t(item.tagKey),
+            title: t(item.titleKey),
+            description: t(item.descriptionKey),
+            img: {
+              ...item.img,
+              alt: t(item.img.altKey),
+            },
           }}
-          data={Array.from({ length: TIMELINE_ITEMS }).map((_, i) => ({
-            type: t(`Home.timeline.items.${i + 1}.type`),
-            year: t(`Home.timeline.items.${i + 1}.year`),
-            date: t(`Home.timeline.items.${i + 1}.date`),
-            title: t(`Home.timeline.items.${i + 1}.title`),
-            description: t(`Home.timeline.items.${i + 1}.description`),
-          }))}
+          reversed={(i + 1) % 2 === 0}
         />
-      </article>
-      <article>
-        <Feedback id="contribute" className="py-[3rem] lg:py-[5.25rem]" />
-      </article>
+      ))}
+      <Timeline
+        id="ourplan"
+        cover={{
+          tag: t("Home.timeline.cover.tag"),
+          title: t("Home.timeline.cover.title"),
+          description: t("Home.timeline.cover.description"),
+        }}
+        data={Array.from({ length: TIMELINE_ITEMS }).map((_, i) => ({
+          type: t(`Home.timeline.items.${i + 1}.type`),
+          year: t(`Home.timeline.items.${i + 1}.year`),
+          date: t(`Home.timeline.items.${i + 1}.date`),
+          title: t(`Home.timeline.items.${i + 1}.title`),
+          description: t(`Home.timeline.items.${i + 1}.description`),
+        }))}
+      />
+      <Feedback id="contribute" className="py-[3rem] lg:py-[5.25rem]" />
+    </Main>
+  );
+}
+
+function Main(props: { children: ReactNode }) {
+  return (
+    <main className="flex w-full flex-col divide-y divide-washed-100 lg:py-0">
+      {React.Children.map(props.children, (child) => (
+        // Wrap each child in <article> to consistently display full-width dividers
+        <article>{child}</article>
+      ))}
     </main>
   );
 }
