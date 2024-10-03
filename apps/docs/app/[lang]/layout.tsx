90d3a72d91a15528d378e8d9f5607cf7d9146a95
irfanismail <irfan.ismail96@gmail.com>
2024-10-04 01:32:08 +0800

    feat: fumadocs init

---
+++ apps/docs/app/[lang]/layout.tsx
@@ -0,0 +1,38 @@
+import "@/app/global.css";
+import { Inter } from "next/font/google";
+import { RootProvider } from "fumadocs-ui/provider";
+import { I18nProvider } from "fumadocs-ui/i18n";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  params,
+  children,
+}: {
+  params: { lang: string };
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang={params.lang} className={inter.className}>
+      <body>
+        <I18nProvider
+          locale={params.lang}
+          locales={[
+            {
+              name: "English",
+              locale: "en",
+            },
+            {
+              name: "Bahasa Melayu",
+              locale: "ms",
+            },
+          ]}
+        >
+          <RootProvider>{children}</RootProvider>
+        </I18nProvider>
+      </body>
+    </html>
+  );
+}
