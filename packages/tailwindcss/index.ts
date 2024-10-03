aeacae2a111b805803b4c6b2319499f11bbf28e6
irfanismail <irfan.ismail96@gmail.com>
2024-10-04 02:11:29 +0800

    feat: @myds/tailwindcss

---
+++ packages/tailwindcss/index.ts
@@ -0,0 +1,9 @@
+// TailwindCSS imports
+export { default as colors } from "tailwindcss/colors";
+export * from "tailwindcss/types/config";
+export * from "tailwindcss/types/generated/colors";
+export * from "tailwindcss/types/generated/corePluginList";
+export * from "tailwindcss/types/generated/default-theme";
+
+// MyDS preset
+export { default as preset } from "./tailwind.config";
