7e6b186b5d9f12505300971972945aaacb514f4d
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2024-10-15 12:25:34 +0800

    fix: add story and copywriting

--- apps/storybook/stories/link.stories.tsx
+++ apps/storybook/stories/link.stories.tsx
@@ -6,13 +6,13 @@ import { createStory } from "../utils";
 
 /**
  * ### Overview
- * The Link component allows you to easily customize anchor elements.
+ * The Link component extends the HTML `<a>` element and allows you to easily customize them.
  *
  * ### Usage
  * ```ts
  * import Link from "@myds/react/link";
  *
- * <Link href="https://design.digital.gov.my">MYDS</Link>
+ * <Link primary href="https://design.digital.gov.my" underline="always">Malaysia's Official Design System</Link>
  * ```
  */
 const meta = {
@@ -58,8 +58,7 @@ const meta = {
           summary: "boolean",
         },
       },
-      description:
-        "Sets the text colour to primary or inherit it from parent.",
+      description: "Sets the text colour to primary or inherit it from parent.",
       control: "boolean",
     },
     underline: {
@@ -83,7 +82,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 /**
- * The default link, used in rich text etc. 
+ * The default link, used in rich text etc.
  */
 export const Default: Story = createStory({
   children: "Malaysia's Official Design System",
@@ -94,8 +93,30 @@ export const Default: Story = createStory({
 });
 
 /**
- * An example using Button with Icon
- */  
+ * To create a link that looks like a button, you can nest the link component in a Button and set the asChild parameter.
+ */
+export const ButtonAsLink: Story = {
+  args: {
+    underline: "none",
+  },
+  parameters: {
+    backgrounds: {
+      default: "light",
+      values: [{ name: "dark", value: "#18181B" }],
+    },
+  },
+  render: (args) => (
+    <Button asChild>
+      <Link {...args}>
+        Ask a question
+      </Link>
+    </Button>
+  ),
+};
+
+/**
+ * An example of a backlink using Button and Icon
+ */
 export const Backlink: Story = {
   args: {
     primary: true,
@@ -119,12 +140,18 @@ export const Backlink: Story = {
   ),
 };
 
+/**
+ * An example of a Link that is underlined when hovered.
+ */
 export const FooterLink: Story = createStory({
   children: "Privacy Policy",
   underline: "hover",
   className: "text-txt-black-700 text-sm",
 });
 
+/**
+ * An example of a Link that has underline disabled.
+ */
 export const NavbarLink: Story = {
   args: {
     underline: "none",
