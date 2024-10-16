7c167e5359410444a20ff424869157309ecfd2b8
harrisazmi <azmi.harris@gmail.com>
2024-10-16 15:56:02 +0800

    chore: initialize checkbox from radix ui

---
+++ packages/react/src/components/checkbox.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from "react";
+
+/**
+ * Props for Checkbox component.
+ * @typedef CheckboxProps
+ * @property {type} propName - Description of propName
+ */
+interface CheckboxProps {
+  // Define your props here
+}
+
+/**
+ * Checkbox component description.
+ * @component
+ * @example
+ * <Checkbox propName="value" />
+ */
+const Checkbox: FunctionComponent<CheckboxProps> = ({
+  // Destructure your props here
+}) => {
+  return (
+    <div>
+      {/* Add your component JSX here */}
+    </div>
+  );
+};
+
+export default Checkbox;
