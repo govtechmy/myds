abcf67984ce616c9bc576f80376135fac1d93936
irfanismail <irfan.ismail96@gmail.com>
2024-11-09 17:05:07 +0800

    chore: remove old web

--- apps/web/src/i18n-config.ts
+++
@@ -1,6 +0,0 @@
-import { LocalePrefix } from "next-intl/routing";
-
-export type Locale = "en-MY" | "ms-MY";
-export const defaultLocale = "en-MY";
-export const locales = [defaultLocale, "ms-MY"];
-export const localePrefix: LocalePrefix = "as-needed";
