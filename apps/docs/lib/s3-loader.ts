4836950623abd403d9cff8dd0f27d72d152a548d
irfanismail <irfan.ismail96@gmail.com>
2024-11-12 17:06:22 +0800

    feat: use cloudfront endpoint; private bucket

--- apps/docs/lib/s3-loader.ts
+++ apps/docs/lib/s3-loader.ts
@@ -2,11 +2,8 @@
 
 interface S3LoaderProps {
   src: string;
-  //* TODO: Cloudfront optims (later)
-  width: number;
-  quality?: number;
 }
 
 export default ({ src }: S3LoaderProps) => {
-  return `https://gnu-myds.s3.ap-southeast-1.amazonaws.com${src}`;
+  return `https://d2391uizq0pg2.cloudfront.net${src}`;
 };
