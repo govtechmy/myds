8307a721f025e5fcec2ca16320f8a6cff921dbbf
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2024-12-11 14:33:56 +0800

    chore: remove unused

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useCallbackRef } from './use-callback-ref';
 export { useControllableState } from './use-controllable-state';
-export { useMediaQuery } from './use-media-query';
 export { default as useToast } from "./use-toast";
