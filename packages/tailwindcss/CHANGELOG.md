e7a94cdb9aef3a68ced58da629c39b70c1f16262
irfanismail <irfan.ismail96@gmail.com>
2024-12-27 22:43:47 +0800

    test

--- packages/tailwindcss/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @govtechmy/myds-css
-
-## 0.0.1-beta.0
-
-### Patch Changes
-
-- trial publish
---
+++ packages/tailwindcss/index.d.ts
@@ -0,0 +1,39 @@
+export type { PresetsConfig as preset } from "tailwindcss/types/config";
+export declare const preset_fumadocs: {
+  light: {
+    background: string;
+    foreground: string;
+    muted: string;
+    "muted-foreground": string;
+    popover: string;
+    "popover-foreground": string;
+    card: string;
+    "card-foreground": string;
+    border: string;
+    primary: string;
+    "primary-foreground": string;
+    secondary: string;
+    "secondary-foreground": string;
+    accent: string;
+    "accent-foreground": string;
+    ring: string;
+  };
+  dark: {
+    background: string;
+    foreground: string;
+    muted: string;
+    "muted-foreground": string;
+    popover: string;
+    "popover-foreground": string;
+    card: string;
+    "card-foreground": string;
+    border: string;
+    primary: string;
+    "primary-foreground": string;
+    secondary: string;
+    "secondary-foreground": string;
+    accent: string;
+    "accent-foreground": string;
+    ring: string;
+  };
+};
