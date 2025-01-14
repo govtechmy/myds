6300a08b2583410689cbf6963c2c7e59cf8d277a
harrisazmi <azmi.harris@gmail.com>
2025-01-14 16:10:40 +0800

    chore: integrate uncontrolled and controlled toggle

--- apps/docs/components/myds.tsx
+++ apps/docs/components/myds.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import React, { useState, ComponentProps, FunctionComponent } from "react";
 import { Button } from "@govtechmy/myds-react/button";
-import { ComponentProps, FunctionComponent } from "react";
+import { Toggle, ToggleThumb } from "./myds";
 export * from "@govtechmy/myds-react/toggle";
 export * from "@govtechmy/myds-react/button";
 export * from "@govtechmy/myds-react/link";
@@ -12,4 +13,25 @@ interface PreviewButtonProps extends ComponentProps<typeof Button> {
 
 export const PreviewButton: FunctionComponent<PreviewButtonProps> = (props) => {
   return <Button {...props} onClick={() => alert(props.pantun)} />;
+};
+
+interface PreviewToggleProps extends ComponentProps<typeof Toggle> {}
+
+export const ControlledToggle: FunctionComponent<PreviewToggleProps> = (
+  props,
+) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-2">
+      <p>Current state: {isChecked ? "On" : "Off"}</p>
+      <Toggle
+        checked={isChecked}
+        onCheckedChange={(checked) => setIsChecked(checked)}
+        {...props}
+      >
+        <ToggleThumb />
+      </Toggle>
+    </div>
+  );
 };
