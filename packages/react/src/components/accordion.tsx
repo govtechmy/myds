c43e4c23b210d5a9a1a81bbb7b474252c119af70
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2025-01-27 14:47:16 +0800

    fix: accordion padding

--- packages/react/src/components/accordion.tsx
+++ packages/react/src/components/accordion.tsx
@@ -68,13 +68,13 @@ const AccordionContent = React.forwardRef<
       "text-txt-black-700 font-body text-body-sm relative overflow-hidden font-normal transition-all duration-300",
       "data-[state=open]:animate-slide-down data-[state=closed]:animate-slide-up",
       "data-[state=closed]:opacity-0 data-[state=open]:opacity-100",
-      "overflow-hidden pb-4 pr-9",
-      className,
     )}
     {...props}
     ref={forwardedRef}
   >
-    {children}
+    <div className={clx("overflow-hidden pb-4 pr-9", className)}>
+      {children}
+    </div>
   </AccordionBase.Content>
 ));
 
