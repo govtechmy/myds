adb61d8342e082bbc49f86724c04c5be61a9731c
harrisazmi <azmi.harris@gmail.com>
2025-02-21 12:04:46 +0800

    chore: add story for toggle theme component'

--- apps/storybook/stories/theme-toggler.stories.tsx
+++ apps/storybook/stories/theme-toggler.stories.tsx
@@ -1,19 +1,22 @@
-import { fn } from "@storybook/test";
 import type { Meta, StoryObj } from "@storybook/react";
-import { createStory } from "../utils";
-import { ThemeToggler } from "@govtechmy/myds-react/theme-toggler";
+import {
+  ThemeToggler,
+  ThemeProvider,
+} from "@govtechmy/myds-react/theme-toggler";
+import { MoonIcon, PlaceholderIcon, SunIcon } from "@govtechmy/myds-react/icon";
+import { clx } from "@govtechmy/myds-react/utils";
 
 /**
  * ### Overview
- * Insert a brief description of the component here.
- *
- * > Insert a ChatGPT pantun here 
- *
+ * The `ThemeToggler` component allows users to toggle between different themes.
+ * It cycles through a list of provided themes, each represented by an icon.
  * ### Usage
  * ```tsx
- * import ThemeToggler from "@govtechmy/myds-react/theme-toggler";
+ * import { ThemeToggler, ThemeProvider } from "@govtechmy/myds-react/theme-toggler";
  *
- * <ThemeToggler />
+ * <ThemeProvider>
+ *   <ThemeToggler />
+ * </ThemeProvider>
  * ```
  */
 const meta = {
@@ -21,34 +24,77 @@ const meta = {
   component: ThemeToggler,
   tags: ["autodocs"],
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
   },
-  args: { onClick: fn() },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
   argTypes: {
-    type: {
+    themes: {
       table: {
-        type: {
-          summary: "enum",
-        },
+        type: { summary: "Themes[]" },
       },
-      description: "insert-description-here",
-      control: "inline-radio",
-      options: ["option-1", "option-2", "option-3"],
+      description:
+        "Array of theme objects containing theme name and associated icon.",
+      control: "object",
     },
-  }
+  },
 } satisfies Meta<typeof ThemeToggler>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 /**
- * Storybook stories for different variants of a component.
- *
- * @example
- * export const Default: Story = createStory({ ... });
- * export const DarkDefault: Story = createStory({ ... , className="dark"}, "dark");
+ * Default ThemeToggler story.
  */
 
-// export const Default: Story = createStory({
-//  children: "Example",
-// });
+export const Default: Story = {
+  render: () => {
+    const htmlClass = document.documentElement.className;
+    return (
+      <div
+        className={clx(
+          htmlClass,
+          "bg-bg-white flex items-center justify-center py-8",
+        )}
+      >
+        <ThemeProvider>
+          <ThemeToggler />
+        </ThemeProvider>
+      </div>
+    );
+  },
+};
+
+/**
+ * ThemeToggler with custom themes.
+ */
+
+export const CustomContent: Story = {
+  render: () => {
+    const htmlClass = document.documentElement.className;
+    return (
+      <div
+        className={clx(
+          htmlClass,
+          "bg-bg-white flex items-center justify-center py-8",
+        )}
+      >
+        <ThemeProvider>
+          <ThemeToggler
+            themes={[
+              { theme: "light", icon: <SunIcon /> },
+              { theme: "dark", icon: <MoonIcon /> },
+              { theme: "pastel", icon: <PlaceholderIcon /> },
+              { theme: "retro", icon: <PlaceholderIcon /> },
+            ]}
+          />
+        </ThemeProvider>
+      </div>
+    );
+  },
+};
