054d7b58be5cc60a5443432695f829d13708d856
harrisazmi <azmi.harris@gmail.com>
2025-03-05 11:33:06 +0800

    fix:responsive text

--- apps/docs/app/[lang]/icon/page.tsx
+++ apps/docs/app/[lang]/icon/page.tsx
@@ -5,7 +5,7 @@ export default function Hello() {
   return (
     <div className="container">
       <div className="from-primary-700 via-primary-300 to-warning-200 my-4 rounded-lg bg-gradient-to-r py-24 text-gray-800">
-        <h1 className="mx-auto max-w-2xl text-center text-5xl">
+        <h1 className="mx-auto max-w-2xl text-center text-5xl max-sm:text-3xl">
           Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.
         </h1>
       </div>
