3ece5d7fcd6c7653c62150f02825192aa0095ebb
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2025-09-23 01:25:52 +0800

    chore: update figma link to official GNU figma

--- apps/docs/lib/constant.ts
+++ apps/docs/lib/constant.ts
@@ -1,6 +1,6 @@
 export const links = {
   figma:
-    "https://www.figma.com/design/svmWSPZarzWrJ116CQ8zpV/MYDS-(Beta)?node-id=7-20696",
+    "https://www.figma.com/design/BwyAzgDaGno8QhaoTtLBMx/MYDS--Beta-?node-id=7-20696&t=xrPkDdDl0w2DqlwX-1",
   standard: "https://standard.digital.gov.my/",
   github: "https://github.com/govtechmy/myds",
   github_issue: "https://github.com/govtechmy/myds/issues",
